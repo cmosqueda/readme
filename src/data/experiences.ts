7600c35e36149646ec15a1541e20c6a07bdd8544
tyne <mosqueda.christinereisa04@gmail.com>
2026-04-25 06:42:41 +0800

    up

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -7,7 +7,7 @@ export const experiences = [
     location: "Cagayan de Oro, PH",
     description:
       "Contributing to the technical layout design and processes, focusing on precision and adherence to publishing standards. In parallel, I also worked on QA testing of an edtech mobile app.",
-    highlights: ["Technical Reporting", "Layout Design", "Mobile App QA Testing"],
+    highlights: ["Layout Design", "Mobile App QA Testing", "Technical Reporting"],
   },
   {
     id: "02",
@@ -16,7 +16,7 @@ export const experiences = [
     period: "March 2025 — Jan 2026",
     location: "Cagayan de Oro, PH",
     description:
-      "Oversaw the end-to-end quality assurance for an AI-powered EdTech platform. Managed a team of 4 to ensure system reliability and usability under ISO/IEC 25010 standards.",
+      "Oversaw the end-to-end process of an AI-powered EdTech platform from planning to deployment and testing. Managed a team of 5 to ensure system reliability and usability under ISO/IEC 25010 standards.",
     highlights: ["ISO/IEC 25010 Evaluation", "SUS Testing (77.95)", "Manual/API Testing"],
   },
 ];
