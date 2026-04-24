10dbb004c2c87f39b34a15fbb3c76f0c06dbc55a
tyne <mosqueda.christinereisa04@gmail.com>
2026-04-25 06:53:09 +0800

    up

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -37,7 +37,7 @@ export default function Sidebar() {
 
           {/* CV BUTTON COMPONENT */}
           <a
-            href="/public/docu/Christine-Mosqueda-CV-updated.pdf"
+            href="/Christine-Mosqueda-CV-updated.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="
